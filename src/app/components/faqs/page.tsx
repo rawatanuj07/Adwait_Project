b3a8a0c11eb88aed91c6a9f442bfa122fe862d7c
Anuj Rawat <rawatanuj07@gmail.com>
2023-10-30 00:36:23 +0530

    ui changes faqs

--- src/app/components/faqs/page.tsx
+++ src/app/components/faqs/page.tsx
@@ -2,12 +2,10 @@
 import React, { PropsWithChildren, useEffect, useState } from "react";
 import { Accordion, AccordionItem } from "@nextui-org/react";
 
-  type YourCourseType = {
-    question: string;
-    answer: string;
-    
-  };
-
+type YourCourseType = {
+  question: string;
+  answer: string;
+};
 
 export default function Faq() {
   const [accordion, setAccordion] = useState<YourCourseType[]>([]);
@@ -19,7 +17,6 @@ export default function Faq() {
       .then((data) => {
         console.log("Akordion is", data);
         setAccordion(data);
-
       });
   }, []);
   const [openAccordion, setOpenAccordion] = useState<string | null>(null);
@@ -32,43 +29,67 @@ export default function Faq() {
     }
   };
   return (
-
-    <div style={{
-    height: "auto",
-    marginTop: "40px",
-    marginBottom: "40px",
-    }}>
-
-      <h1 style={{
-    height: "auto",
-    marginLeft: "40px",
-    marginTop: "40px",
-    marginBottom: "0px",
-    }}>FAQs</h1>
-      <h1>Can’t find the answer you’re looking for? Reach out to our support team.</h1>
-    <div style={{
-        marginLeft: "40%",
-        marginRight: "auto",
-       width: "700px",
-       marginTop: "40px",
-       marginBottom: "40px",
-      }}>
-        
-      {accordion.map((faqss: any) => (
-        <Accordion key={faqss.question}>
-          <AccordionItem style={{
-      
-       marginTop: "40px",
-       marginBottom: "40px",
-      }} key={faqss.id} title={faqss.question}
-
+    <div
+      style={{
+        height: "auto",
+        marginTop: "43px",
+        marginBottom: "43px",
+      }}
+    >
+      {/*  ACCORDION STARTS */}
+      <div
+        style={{
+          marginLeft: "40%",
+          marginRight: "auto",
+          width: "700px",
+       
+        }}
+      >
+        <div
+          style={{
+            width: "400px",
+            marginLeft: "-70%",
+            paddingTop: "10px",
+            marginBottom: "-200px",
+          }}
+        >
+          <h1
+            style={{
+              fontWeight: "bold",
+              fontSize: "20px",
+              marginTop: "50px",
+            }}
           >
-            {faqss.answer.replace(/<\/?p>/g, "")}
-          </AccordionItem>
-        </Accordion>
-      ))}
+            FAQs
+          </h1>
+          <h1
+            style={{
+              marginTop: "10px",
+              fontWeight: "normal",
+              fontSize: "16px",
+            }}
+          >
+            Can’t find the answer you’re looking for?
+          </h1>
+          <h1> Reach out to our <a href="https://acharyaprashant.org/en/technical-support" style={{ color: "#de6b1f" }}>support</a> team.</h1>        </div>
+        {accordion.map((faqss: any, index: number) => (
+          <Accordion key={faqss.question}>
+            <AccordionItem
+              style={{
+                marginTop: "40px",
+                marginBottom: "40px",
+                borderBottom: index !== accordion.length - 1 ? "0.5px solid #ccc" : "",
+                paddingBottom: "20px",
+              }}
+              key={faqss.id}
+              title={faqss.question}
+            >
+              {faqss.answer.replace(/<\/?p>/g, "")}
+            </AccordionItem>
+          </Accordion>
+        ))}
+      </div>
+      {/*  ACCORDION Ends */}
     </div>
-    </div>
-
   );
 }
